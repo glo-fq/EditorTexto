d2f2ae40509d80c88006794eb4369c1bde9b87e2
Gloriana Fernandez <glorianafernandezq@hotmail.com>
2019-11-06 11:08:03 -0600

    Cambios pdf y xml

--- EditorTexto/Model/ArchivoPDF.cs
+++ EditorTexto/Model/ArchivoPDF.cs
@@ -11,11 +11,13 @@ namespace EditorTexto.Model
         //Antes de guardar
         public override string convertirAFormatoDeseado(Texto texto)
         {
+            //byte[] bytes = Encoding.UTF8.GetBytes(texto.getText());
             throw new NotImplementedException();
         }
         //Despues de abrir
         public override string convertirATexto(string textoArchivo)
         {
+            
             throw new NotImplementedException();
         }
     }
--- EditorTexto/Model/ArchivoXML.cs
+++ EditorTexto/Model/ArchivoXML.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EditorTexto.Model
 {
@@ -11,12 +13,79 @@ namespace EditorTexto.Model
         //Antes de guardar
         public override string convertirAFormatoDeseado(Texto texto)
         {
-            throw new NotImplementedException();
+            RichTextBox rtb = texto.getRich();
+
+            rtb.HideSelection = true;
+
+            string strFinal = "<colores>\n";
+
+            //string colorAnterior = "";
+            int colorAnterior = 0;
+
+            for (int i = 0; i < texto.getText().Length; i++) {
+                rtb.Select(i, i);
+                string charac = rtb.SelectedText.ToString();
+
+
+
+                //string color = getNombreColor(rtb.SelectionColor);
+                int color = rtb.SelectionColor.ToArgb();
+                Console.WriteLine(i + ". Color: " + rtb.SelectionColor.ToArgb().ToString());
+
+
+                if (colorAnterior != color) {
+                strFinal += "<" + color + ">" + i + "</" + color + ">\n";
+                }
+                
+
+                colorAnterior = color;
+
+            }
+
+            strFinal += "</colores>\n";
+            strFinal += "<texto>\n";
+            strFinal += texto.getText();
+            strFinal += "\n"; 
+            strFinal += "</texto>";
+
+            rtb.DeselectAll();
+
+            rtb.HideSelection = false;
+
+            return strFinal;
+        }
+
+        private string quitarWhitespace(string texto) {
+            return new string(texto.ToCharArray().Where(c => !Char.IsWhiteSpace(c)).ToArray());
+        }
+
+        private string getNombreColor(Color colorToCheck) {
+            string name = "Unknown";
+            foreach (KnownColor kc in Enum.GetValues(typeof(KnownColor)))
+            {
+                int azul = Color.Blue.ToArgb();
+                //Console.WriteLine(azul.ToString());
+                int rojo = Color.Red.ToArgb();
+                int verde = Color.Green.ToArgb();
+
+                Color known = Color.FromKnownColor(kc);
+                //Console.WriteLine(known.Name);
+                if (colorToCheck.ToArgb().Equals(known.ToArgb()))
+                {
+                    name = known.Name;
+
+                }
+                
+                
+            }
+            return name;
         }
 
         //Despues de abrir
         public override string convertirATexto(string textoArchivo)
         {
+            var pieces = textoArchivo.Split(new[] { "</colores>" }, StringSplitOptions.None);
+
             throw new NotImplementedException();
         }
     }
