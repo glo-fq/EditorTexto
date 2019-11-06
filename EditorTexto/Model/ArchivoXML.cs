811c632c1c43edc5c173710b914c8c2a21b7939c
Gloriana Fernandez <glorianafernandezq@hotmail.com>
2019-11-06 13:19:07 -0600

    Leer xml

--- EditorTexto/Model/ArchivoXML.cs
+++ EditorTexto/Model/ArchivoXML.cs
@@ -19,18 +19,18 @@ namespace EditorTexto.Model
 
             string strFinal = "<colores>\n";
 
-            //string colorAnterior = "";
-            int colorAnterior = 0;
+            string colorAnterior = "";
+            //int colorAnterior = 0;
 
             for (int i = 0; i < texto.getText().Length; i++) {
-                rtb.Select(i, i);
+                rtb.Select(i, 1);
                 string charac = rtb.SelectedText.ToString();
 
 
 
-                //string color = getNombreColor(rtb.SelectionColor);
-                int color = rtb.SelectionColor.ToArgb();
-                Console.WriteLine(i + ". Color: " + rtb.SelectionColor.ToArgb().ToString());
+                string color = getNombreColor(rtb.SelectionColor);
+                //int color = rtb.SelectionColor.ToArgb();
+                //Console.WriteLine(i + ". Color: " + rtb.SelectionColor.ToArgb().ToString());
 
 
                 if (colorAnterior != color) {
@@ -81,12 +81,35 @@ namespace EditorTexto.Model
             return name;
         }
 
+        public RichTextBox agregarColores(Texto texto) {
+
+            RichTextBox rtf = texto.getRich();
+
+            var pieces = texto.getText().Split(new[] { "</colores>" }, StringSplitOptions.None);
+            string colores = pieces[0];
+
+            throw new NotImplementedException();
+        }
+
         //Despues de abrir
         public override string convertirATexto(string textoArchivo)
         {
             var pieces = textoArchivo.Split(new[] { "</colores>" }, StringSplitOptions.None);
 
-            throw new NotImplementedException();
+            //string colores = pieces[0];
+            string texto = pieces[1];
+
+            //Quita los tags de colores
+            //colores.Remove(0, 8);
+            //colores.Substring(colores.Length - 9);
+
+            //Quita los tags de texto
+            texto = texto.Remove(0, 9);
+            texto = texto.Substring(0, texto.Length - 11);
+
+            return texto;
+
+            //throw new NotImplementedException();
         }
     }
 }
